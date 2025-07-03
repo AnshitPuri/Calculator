e1c2b5a65354d10af4cd50f43775b0c06b50abc8
Anshit <anshitpuri99@gmail.com>
2025-07-03 18:14:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,12 @@
-let currentDisplay = document.querySelector('#textAreaText')
+const currentDisplay = document.querySelector('#textAreaText')
 
-let buttonEqual = document.querySelector('#buttonEqual')
+const buttonEqual = document.querySelector('#buttonEqual')
 
-let buttonAc = document.querySelector('#buttonAc')
+const buttonAc = document.querySelector('#buttonAc')
 
-let buttonDel = document.querySelector('#buttonDel')
+const buttonDel = document.querySelector('#buttonDel')
 
-buttonEqual.onclick = () =>{
-    adjustFontSize();
-    if(currentDisplay.textContent.trim() === ""){
-        currentDisplay.textContent = "0"
-        return
-    }
-    try{
-        let result = eval(currentDisplay.textContent)
-        currentDisplay.textContent= parseFloat(result.toFixed(8))
-    }
-    catch{
-        currentDisplay.textContent = "Error"
-    }
-}
-
-buttonAc.onclick = () => {
-    adjustFontSize();
-    currentDisplay.textContent = ""
-}
-
-buttonDel.onclick = () => {
-    adjustFontSize();
-    if(currentDisplay.textContent === "Error"){
-        currentDisplay.textContent = ''
-    }
-    else{
-        currentDisplay.textContent = currentDisplay.textContent.slice(0,-1)
-    }
-}
+const button = document.querySelectorAll('.button')
 
 function adjustFontSize() {
     const length = currentDisplay.textContent.length;
@@ -49,3 +21,36 @@ function adjustFontSize() {
         currentDisplay.style.fontSize = "36px";
     }
 }
+
+buttonEqual.onclick = () => {
+    if (currentDisplay.textContent.trim() === "") {
+        currentDisplay.textContent = "0"
+        return
+    }
+    try {
+        let result = eval(currentDisplay.textContent)
+        currentDisplay.textContent = parseFloat(result.toFixed(8))
+    }
+    catch {
+        currentDisplay.textContent = "Error"
+    }
+}
+
+buttonAc.onclick = () => {
+    currentDisplay.textContent = ""
+}
+
+buttonDel.onclick = () => {
+    if (currentDisplay.textContent === "Error") {
+        currentDisplay.textContent = ''
+    }
+    else {
+        currentDisplay.textContent = currentDisplay.textContent.slice(0, -1)
+    }
+}
+
+button.forEach((button) => {
+    button.onclick = () => {
+        adjustFontSize()
+    }
+})
